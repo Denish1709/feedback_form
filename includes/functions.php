108eb89e931939d8ba317cfe00e4cd043a6a99b6
Denish <denishrao22s@forwardschool.edu.my>
2023-05-29 22:17:50 +0800

    Initial commit

---
+++ includes/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function connectToDB() {
+    $host = 'devkinsta_db'; // change this to devkinsta_db
+    $dbname = 'feedback'; // use your own database name
+    $dbuser = 'root';
+    $dbpassword = 'aqvEwR9D41FvwC6l'; // use your own password
+
+    $database = new PDO (
+        "mysql:host=$host;dbname=$dbname",
+        $dbuser,
+        $dbpassword
+    );
+
+    return $database;
+}
